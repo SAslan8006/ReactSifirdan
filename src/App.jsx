e8a690d7e40db68a31151817cd5453178a3db279
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-10-17 15:24:29 +0300

    State useState Hookları

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import './App.css'
 
 function App() {
+  const name = "Süleyman"
 
+  const data = [
+    { id: 1, name: "süleyman" },
+    { id: 2, name: "süleyman2" },
+    { id: 3, name: "süleyman3" },
+  ]
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
+      <h1 className="text-3xl font-bold justify-center">
+        {data.map((item) => (
+          <div className="text-3xl font-bold mt-3 bg-red-400 justify-center items-center rounded-md" key={item.id}>{item.name}</div>
+        ))}
       </h1>
+      <h1 className='text-3xl text-red-400 font-bold cursor-pointer'> {name}</h1>
 
     </>
   )
