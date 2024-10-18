19df7c3e5dfe593b738a3cfc081ce247c65a0474
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-10-18 14:08:44 +0300

    MyContextProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+import MyContextProvider from "./context/MyContext.jsx"
 createRoot(document.getElementById('root')).render(
-  <>
+  <MyContextProvider>
     <App />
-  </>,
+  </MyContextProvider>,
 )
