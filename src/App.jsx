6c1d46494f1a301fb1a6792e8102acdc84450d3b
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-10-21 14:44:46 +0300

    Projeler

--- src/App.jsx
+++ src/App.jsx
@@ -1,117 +1,74 @@
-import { useRef, useEffect, useState } from 'react';
-import jsQR from 'jsqr'; // Alternatif kütüphane ekleniyor
+import axios from "axios";
+import { useEffect } from "react";
 
+const BASE_Url = "http://localhost:4000";
 function App() {
-  const video = useRef(null);
-  const canvas = useRef(null);
-  const [barcode, setBarcode] = useState(null);
-  const [basket, setBasket] = useState([]);
-  const [intervalId, setIntervalId] = useState(null);
 
-  const openCam = () => {
-    navigator.mediaDevices.getUserMedia({
-      video: {
-        width: 300,
-        height: 300,
-        facingMode: 'environment' // mobilde arka kamerayı açtırmak için
-      }
-    })
-      .then(stream => {
-        video.current.srcObject = stream;
-        video.current.play();
-
-        const ctx = canvas.current.getContext('2d');
-
-        if ('BarcodeDetector' in window) {
-          const barcodeDetector = new window.BarcodeDetector({ formats: ['qr_code', 'ean_13'] });
-
-          const id = setInterval(() => {
-            canvas.current.width = video.current.videoWidth;
-            canvas.current.height = video.current.videoHeight;
-            ctx.drawImage(video.current, 0, 0, video.current.videoWidth, video.current.videoHeight);
-
-            barcodeDetector.detect(canvas.current)
-              .then((detectedCodes) => {
-                if (detectedCodes && detectedCodes.length > 0) {
-                  setBarcode(detectedCodes[0].rawValue);
-                }
-              })
-              .catch(err => console.log(err));
-          }, 100);
-
-          setIntervalId(id);
-        } else {
-          // BarcodeDetector yoksa jsQR ile algılama yapılıyor
-          const id = setInterval(() => {
-            canvas.current.width = video.current.videoWidth;
-            canvas.current.height = video.current.videoHeight;
-            ctx.drawImage(video.current, 0, 0, video.current.videoWidth, video.current.videoHeight);
-
-            const imageData = ctx.getImageData(0, 0, canvas.current.width, canvas.current.height);
-            const code = jsQR(imageData.data, canvas.current.width, canvas.current.height);
-
-            if (code) {
-              setBarcode(code.data);
-            }
-          }, 100);
-
-          setIntervalId(id);
-        }
-      })
-      .catch(err => alert("Kamera erişimi sağlanamadı: " + err));
+  const getAllUser = async () => {
+    try {
+      const response = await axios.get(`${BASE_Url}/users`);
+      console.log(response.data);  // Kullanıcı verileri burada
+      // setUsers(response.data);  // (React kullanıyorsanız)
+    } catch (error) {
+      console.error("Bir hata oluştu:", error);
+      alert("Kullanıcılar yüklenemedi. Lütfen daha sonra tekrar deneyiniz.");
+    }
+  };
+  const geTUserById = async (userId) => {
+    try {
+      const response = await axios.get(`${BASE_Url}/users/${userId}`);
+      console.log(response.data);  // Kullanıcı verileri burada
+      // setUsers(response.data);  // (React kullanıyorsanız)
+    } catch (error) {
+      console.error("Bir hata oluştu:", error);
+      alert("Kullanıcılar yüklenemedi. Lütfen daha sonra tekrar deneyiniz.");
+    }
+  };
+  const createUser = async (newUser) => {
+    try {
+      const response = await axios.post(`${BASE_Url}/users`, newUser);
+      console.log(response.data);  // Kullanıcı verileri burada
+      // setUsers(response.data);  // (React kullanıyorsanız)
+    } catch (error) {
+      console.error("Bir hata oluştu:", error);
+      alert("Kullanıcılar yüklenemedi. Lütfen daha sonra tekrar deneyiniz.");
+    }
   };
 
-  useEffect(() => {
-    if (barcode) {
-      const API_URL = `http://localhost/api.php?barcode=${barcode}`;
+  const updateUser = async (userId, updatedUser) => {
+    try {
+      const response = await axios.put(`${BASE_Url}/users/${userId}`, updatedUser);
+      console.log(response.data);  // Kullanıcı verileri burada
 
-      fetch(API_URL)
-        .then(res => res.json())
-        .then(data => {
-          if (data) {
-            setBasket(prevBasket => [...prevBasket, data]);
-          } else {
-            alert('Bu ürün bulunamadı!');
-          }
-        })
-        .catch(err => console.log(err));
+    } catch (error) {
+      console.error("Bir hata oluştu:", error);
+      alert("Kullanıcılar yüklenemedi. Lütfen daha sonra tekrar deneyiniz.");
     }
-  }, [barcode]);
-
+  }
+  // const newUser = {
+  //   "firstName": "jone",
+  //   "lastName": "mac",
+  //   "username": "ali5",
+  //   "phoneNumber": "111111111",
+  //   "emailAddress": "jone.mac@learningcontainer.com",
+  //   "password": "123456"
+  // }
   useEffect(() => {
-    return () => {
-      // Kamera akışını durdurma
-      if (video.current && video.current.srcObject) {
-        const tracks = video.current.srcObject.getTracks();
-        tracks.forEach(track => track.stop());
-      }
-
-      // setInterval temizleme
-      if (intervalId) {
-        clearInterval(intervalId);
-      }
-    };
-  }, [intervalId]);
+    // getAllUser();  // Fonksiyonu çalıştırmak
+    // createUser(newUser);
+    // updateUser("093a", {
+    //   "firstName": "jone",
+    //   "lastName": "mac",
+    //   "username": "ali23",
+    //   "phoneNumber": "111111111",
+    //   "emailAddress": "jone.mac@learningcontainer.com",
+    //   "password": "123456"
+    // })
+  }, [])
 
   return (
     <>
-      <button onClick={openCam}>Kamerayı Aç</button>
-      <div>
-        <video ref={video} autoPlay muted hidden />
-        <canvas ref={canvas} />
-      </div>
-      {barcode && (
-        <div>
-          Bulunan barkod: {barcode}
-        </div>
-      )}
-      {basket.length > 0 && basket.map(item => (
-        <div key={item.id}>
-          {item.product} <br />
-          {item.price} ₺ <br />
-          <img src={item.image} alt={item.product} style={{ width: 100, height: 100 }} />
-        </div>
-      ))}
+
     </>
   );
 }
