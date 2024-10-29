705ce547c96803a42a1cc241eb12724db4482565
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-10-29 14:47:24 +0300

    Düzenleme Liste

--- RouterDomm/src/App.jsx
+++ RouterDomm/src/App.jsx
@@ -1,13 +1,28 @@
 import { useState } from 'react'
 import './App.css'
-
+import { Route, Routes } from 'react-router-dom'
+import Header from './components/Header.jsx'
+import Home from './pages/Home.jsx'
+import About from './pages/About';
+import Employe from './pages/Employe';
+import NotFoundPage from './pages/NotFoundPage.jsx'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />}>
+          <Route path='employee' element={<Employe />} />
+        </Route>
 
-    </>
+        <Route path="*" element={<NotFoundPage />} />
+
+      </Routes>
+    </div>
+
   )
 }
 
---
+++ RouterDomm/src/pages/Employe.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Employe = () => {
+    return (
+        <div>
+            Employe
+        </div>
+    )
+}
+
+export default Employe
