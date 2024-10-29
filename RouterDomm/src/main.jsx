0543294494c5fb58e8a786f56dea954dc011153a
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-10-29 10:57:08 +0300

    Nested Router

--- RouterDomm/src/main.jsx
+++ RouterDomm/src/main.jsx
@@ -1,13 +1,13 @@
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
-import { store } from './redux/store.js'
-import { Provider } from 'react-redux'
+import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
+  <StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>,
+  </StrictMode>,
 )
