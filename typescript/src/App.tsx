63ffd1f14709c9899641579c40e0d1fb84912dfe
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-11-01 14:22:10 +0300

    type Script

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,6 +6,13 @@ function App() {
   //Union Types
   //  let age:string|number = 25;
   //  let name: string|number = 'John';
+  // String , boolean , number
+  // interface- type: kendi tipimizi oluşturmamızı sağlıyor. 
+  type user{
+    name: string,
+    age: number,
+    email: string
+  }
 
   return (
     <>
