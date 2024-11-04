21494bc1ebb633c700690d08c9a1fee31fbd3cf1
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-11-04 17:38:32 +0300

    Counter Hook

--- hook/src/App.jsx
+++ hook/src/App.jsx
@@ -1,11 +1,13 @@
-import { useState } from 'react'
 import './App.css'
+import useCounter from './hooks/useCounter'
 
 function App() {
-
+  const { count, increment, decrement } = useCounter();
   return (
     <>
-    
+      <div>{count}</div>
+      <button onClick={increment}>Artır</button>
+      <button onClick={decrement}>Azalt</button >
     </>
   )
 }
