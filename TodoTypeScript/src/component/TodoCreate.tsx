036dcad2d9670e4a6d7c33d24151557bf54a8e4c
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-11-04 10:04:57 +0300

    TodoTypeScrit Başlangıç

---
+++ TodoTypeScript/src/component/TodoCreate.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TodoCreate = () => {
+  return (
+    <div className='todo-create'>
+        <input placeholder='Todo Giriniz...' className='todo-input' type="text" />
+        <button className='todo-create-button '>Todo Ekle</button>
+      
+    </div>
+  )
+}
+
+export default TodoCreate
