9c26d621b74e75785abb399b6f9f416e816e5e3f
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-11-05 09:31:31 +0300

    Copyalama Use Hook ile

--- hook/src/App.jsx
+++ hook/src/App.jsx
@@ -1,17 +1,22 @@
 import './App.css'
 import useCounter from './hooks/useCounter'
 import useToggle from './hooks/useToggle';
+import useCopyToClipboard from './hooks/useCopyToClipboard';
 
 function App() {
   const { count, increment, decrement } = useCounter();
   const { chage, isToggled } = useToggle();
+  const [copied, copy] = useCopyToClipboard("Merhaba");
 
   return (
     <>
       {isToggled && <div>Merhaba</div>} <div>{count}</div>
+      {copied && <div>Kopyalandı</div>}
       <button onClick={increment}>Artır</button>
-      <button onClick={chage}>Artır</button>
+      <button onClick={chage}>Göster/Gizle</button>
       <button onClick={decrement}>Azalt</button >
+      <button onClick={copy}>Kopyala</button>
+
     </>
   )
 }
