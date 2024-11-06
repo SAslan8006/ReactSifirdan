c4635f44703c98acf05b266cec89ad177a496b3c
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-11-06 16:54:48 +0300

    Card Kullanımı

--- matarialUI/src/App.jsx
+++ matarialUI/src/App.jsx
@@ -7,12 +7,13 @@ import MUITextFiedl from './component/MUITextFiedl'
 import MUIBox from './component/MUIBox';
 import MUIStack from './component/MUIStack'
 import MUIGrid from './component/MUIGrid'
+import MUICard from './component/MUICard'
 
 function App() {
 
   return (
     <div>
-      <MUIGrid />
+      <MUICard />
     </div>
   )
 }
