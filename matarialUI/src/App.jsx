3fccdb08b63401854b0c56eb3c3412a6e80a54d3
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-11-06 13:05:07 +0300

    MuıSelect

--- matarialUI/src/App.jsx
+++ matarialUI/src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import MUIButton from './component/MUIButton'
+import MUISelect from './component/MUISelect'
 import MUITextFiedl from './component/MUITextFiedl'
 
 function App() {
 
   return (
     <div>
-      <MUITextFiedl />
+      <MUISelect />
     </div>
   )
 }
---
+++ matarialUI/src/component/MUISelect.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import Box from '@mui/material/Box';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+function MUISelect() {
+    const [age, setAge] = React.useState('');
+
+    const handleChange = (event) => {
+        setAge(event.target.value);
+    };
+    return (
+        <Box sx={{ minWidth: 120 }}>
+            <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">Para Birimi</InputLabel>
+                <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={age}
+                    label="Age"
+                    onChange={handleChange}
+                >
+                    <MenuItem value="USD">Dolar</MenuItem>
+                    <MenuItem value="EUR">EURO</MenuItem>
+                    <MenuItem value="TRY">Türk Lirası</MenuItem>
+                </Select>
+            </FormControl>
+        </Box>
+    )
+}
+
+export default MUISelect
