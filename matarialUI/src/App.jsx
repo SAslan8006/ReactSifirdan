e4ca7bed6261351a2a658b89b04f7f0326c8a205
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-11-06 14:05:06 +0300

    MUIAutocomplete

--- matarialUI/src/App.jsx
+++ matarialUI/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import MUIAutocomplete from './component/MUIAutocomplete'
 import MUIButton from './component/MUIButton'
 import MUICheckBox from './component/MUICheckBox'
 import MUISelect from './component/MUISelect'
@@ -8,7 +9,7 @@ function App() {
 
   return (
     <div>
-      <MUICheckBox />
+      <MUIAutocomplete />
     </div>
   )
 }
