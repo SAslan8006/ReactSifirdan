5112d7f28123fe79e635bdde340f68744dcf8235
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-11-06 13:21:45 +0300

    Select

--- matarialUI/src/component/MUISelect.jsx
+++ matarialUI/src/component/MUISelect.jsx
@@ -4,30 +4,53 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+import { TextField } from '@mui/material';
 
 function MUISelect() {
     const [age, setAge] = React.useState('');
+    const [ages, setAges] = React.useState([]);
 
     const handleChange = (event) => {
         setAge(event.target.value);
+        setAges(event.target.value);
     };
     return (
-        <Box sx={{ minWidth: 120 }}>
-            <FormControl fullWidth>
-                <InputLabel id="demo-simple-select-label">Para Birimi</InputLabel>
-                <Select
-                    labelId="demo-simple-select-label"
-                    id="demo-simple-select"
-                    value={age}
-                    label="Age"
-                    onChange={handleChange}
-                >
-                    <MenuItem value="USD">Dolar</MenuItem>
-                    <MenuItem value="EUR">EURO</MenuItem>
-                    <MenuItem value="TRY">Türk Lirası</MenuItem>
-                </Select>
-            </FormControl>
-        </Box>
+        <div>
+            <Box sx={{ minWidth: 120 }}>
+                <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label">Para Birimi</InputLabel>
+                    <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={age}
+                        label="Age"
+                        onChange={handleChange}
+                    >
+                        <MenuItem value="USD">Dolar</MenuItem>
+                        <MenuItem value="EUR">EURO</MenuItem>
+                        <MenuItem value="TRY">Türk Lirası</MenuItem>
+                    </Select>
+                </FormControl>
+            </Box>
+            <TextField sx={{ minWidth: 120, marginTop: '20px' }} label="Para Birimi" select value={age} onChange={handleChange} color="success" >
+                <MenuItem value="USD">Dolar</MenuItem>
+                <MenuItem value="EUR">EURO</MenuItem>
+                <MenuItem value="TRY">Türk Lirası</MenuItem>
+            </TextField>
+
+            <TextField
+                sx={{ minWidth: 120, marginTop: '20px' }}
+                label="Para Birimi"
+                SelectProps={{ multiple: true }}
+                value={ages}
+                onChange={handleChange}
+                color="success" >
+                <MenuItem value="USD">Dolar</MenuItem>
+                <MenuItem value="EUR">EURO</MenuItem>
+                <MenuItem value="TRY">Türk Lirası</MenuItem>
+            </TextField>
+
+        </div>
     )
 }
 
