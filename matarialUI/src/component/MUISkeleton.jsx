aa27a0282f2a26460d12dc8e737054350aadd0a4
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-11-11 11:47:15 +0300

    MUISkeleton Kullanımı

--- matarialUI/src/component/MUISkeleton.jsx
+++ matarialUI/src/component/MUISkeleton.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Skeleton from '@mui/material/Skeleton';
+import Stack from '@mui/material/Stack';
 
 function MUISkeleton() {
+    const [loading, setLoading] = React.useState(true);
+    //resimleri 2.5 saniye sonra yüklüyor hissi veren skeleton yapıldı
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false)
+        }, 2500)
+    }, [])
     return (
-        <div>MUISkeleton</div>
+        <div>
+            {loading ? (<Skeleton
+                animation="wave"
+                variant="rounded"
+                width={250}
+                height={150} />) : (<img width={250} height={150} src='https://images.unsplash.com/photo-1551963831-b3b1ca40c98e' />)}
+        </div>
     )
 }
 
