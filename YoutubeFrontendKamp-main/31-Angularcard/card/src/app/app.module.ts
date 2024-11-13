ba91fc2a9f0dca7b37a38dc5f8ee89c333b65e42
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-11-13 10:10:42 +0300

    Can Bey Eğitim Frontend

---
+++ YoutubeFrontendKamp-main/31-Angularcard/card/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { CardComponent } from './card/card.component';
+import { BlogDialogComponent } from './home/blog-dialog/blog-dialog.component';
+
+@NgModule({
+  declarations: [AppComponent, CardComponent, BlogDialogComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
