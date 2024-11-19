fc55240bc9b4566acd88e00ae3582d1b83b8c31d
Süleyman ASLAN <suleymanaslan.8006@gmail.com>
2024-11-19 12:01:08 +0300

    Delete Button ve Todolist Line

---
+++ todoTypeScriptLogin/src/components/DeleteButton.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function DeleteButton() {
+  return (
+    <div>
+      <button>❌</button>
+    </div>
+  );
+}
+
+export default DeleteButton;
--- todoTypeScriptLogin/src/components/TodoList.tsx
+++ todoTypeScriptLogin/src/components/TodoList.tsx
@@ -1,9 +1,44 @@
 import React from "react";
+import DeleteButton from "./DeleteButton";
 
+const todos = [
+  {
+    id: 1,
+    content: "Todo 1",
+    completed: true,
+  },
+  {
+    id: 2,
+    content: "Todo 2",
+    completed: false,
+  },
+  {
+    id: 3,
+    content: "Todo 3",
+    completed: true,
+  },
+];
 function TodoList() {
   return (
     <div>
-      <ul></ul>
+      <ul>
+        {todos.map((todo) => (
+          <div key={todo.id}>
+            <li
+              className={`flex justify-between items-center px-8 h-[50px] text-[14px] cursor-pointer border-b border-b-[rgba(0,0,0,0.08)]`}
+            >
+              <span
+                className={`${
+                  todo.completed ? "line-through text-[#ccc]" : ""
+                }`}
+              >
+                {todo.content}
+              </span>
+              <DeleteButton />
+            </li>
+          </div>
+        ))}
+      </ul>
     </div>
   );
 }
